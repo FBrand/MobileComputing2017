928736a360962a64bf257c8b5d39f1b21ef27321
Yorrick <yreinhar@students.uni-mainz.de>
2017-03-07 11:31:55 +0100

    Name des Vereins korrigiert

---
+++ SCLerchenbergTrainerAssist/app/src/main/java/com/scltrainer/uni_mainz/sclerchenbergtrainerassist/MainActivity.java
@@ -0,0 +1,13 @@
+package com.scltrainer.uni_mainz.sclerchenbergtrainerassist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
