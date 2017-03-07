ca1df54c49d5bb757d6f2d4b24c52685500c42de
Yorrick <yreinhar@students.uni-mainz.de>
2017-03-07 14:46:55 +0100

    Simples Fragment als Hauptmenü integriert, bestehend aus einer simple-List

--- SCLerchenbergTrainerAssist/app/src/main/java/com/scltrainer/uni_mainz/sclerchenbergtrainerassist/MainActivity.java
+++ SCLerchenbergTrainerAssist/app/src/main/java/com/scltrainer/uni_mainz/sclerchenbergtrainerassist/MainActivity.java
@@ -2,12 +2,15 @@ package com.scltrainer.uni_mainz.sclerchenbergtrainerassist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("APP", "onCreate start");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("APP", "onCreate ende");
     }
 }
---
+++ SCLerchenbergTrainerAssist/app/src/main/java/com/scltrainer/uni_mainz/sclerchenbergtrainerassist/MenuFragment.java
@@ -0,0 +1,45 @@
+package com.scltrainer.uni_mainz.sclerchenbergtrainerassist;
+
+import android.annotation.SuppressLint;
+import android.app.ListFragment;
+import android.os.Bundle;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+public class MenuFragment extends ListFragment implements OnItemClickListener {
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        Log.i("APP", "onCreateView start");
+        View view = inflater.inflate(R.layout.fragment_menu, container, false);
+        Log.i("APP", "onCreate ende");
+        return view;
+
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.i("APP", "onActivityCreated start");
+
+        // TODO: Eigenen ArrayAdapter schreiben um eine schoenere Liste zu erzeugen
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.menu, android.R.layout.simple_list_item_1);
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+        Log.i("APP", "onActivityCreated ende");
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,long id) {
+        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
+    }
+}
