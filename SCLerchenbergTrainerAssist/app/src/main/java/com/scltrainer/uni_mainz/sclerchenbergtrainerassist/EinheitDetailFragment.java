9e4d020aede66810824e73c075ce3e3b0feab7cc
Yorrick <yreinhar@students.uni-mainz.de>
2017-03-09 12:25:38 +0100

    EinheitDetail Klassen erzeugt. Fragment_uebung.xml ergänzt mit CardView.

---
+++ SCLerchenbergTrainerAssist/app/src/main/java/com/scltrainer/uni_mainz/sclerchenbergtrainerassist/EinheitDetailFragment.java
@@ -0,0 +1,22 @@
+package com.scltrainer.uni_mainz.sclerchenbergtrainerassist;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class EinheitDetailFragment extends Fragment {
+
+    public EinheitDetailFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_einheit_detail, container, false);
+    }
+}
