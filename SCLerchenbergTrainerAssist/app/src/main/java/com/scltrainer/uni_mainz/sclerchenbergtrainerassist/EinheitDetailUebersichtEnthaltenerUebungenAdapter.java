96ff8689316ec478e40491283786dcef71c06f4d
Yorrick <yreinhar@students.uni-mainz.de>
2017-03-16 14:48:05 +0100

    Majas Klasse hinzugefügt, sie streikt aber stark.

---
+++ SCLerchenbergTrainerAssist/app/src/main/java/com/scltrainer/uni_mainz/sclerchenbergtrainerassist/EinheitDetailUebersichtEnthaltenerUebungenAdapter.java
@@ -0,0 +1,133 @@
+package com.scltrainer.uni_mainz.sclerchenbergtrainerassist;
+import android.content.Context;
+
+import android.database.Cursor;
+
+import android.util.Log;
+
+import android.view.LayoutInflater;
+
+import android.view.View;
+
+import android.view.ViewGroup;
+
+import android.widget.CursorAdapter;
+
+import android.widget.RatingBar;
+
+import android.widget.TextView;
+
+
+
+import java.util.ArrayList;
+
+
+
+public class EinheitDetailUebersichtEnthaltenerUebungenAdapter extends CursorAdapter {
+
+
+
+    private ArrayList<Integer> ids = new ArrayList<Integer>();
+
+    private LayoutInflater inflator;
+
+    //private int s;
+
+    String TAG = "EDUEUAdapter";
+
+
+
+    /**
+     * Dient zum Speichern der Reihenfolge der Ids der ListView-Einträge
+     * um die Detailansicht öffnen zu können.
+     */
+    private ArrayList<Integer> idList;
+
+
+
+    public EinheitDetailUebersichtEnthaltenerUebungenAdapter(Context context, Cursor c) {
+
+        super(context, c);
+
+        inflator = LayoutInflater.from(context);
+
+        //s = c.getCount();
+
+        idList = new ArrayList<Integer>();
+
+    }
+
+
+
+    public int getIdListEntry(int entry){
+
+        return idList.get(entry);
+
+    }
+
+
+
+
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+
+
+        if(cursor == null) return;
+
+
+        ids.add(cursor.getInt(0));
+
+        TextView uebungenListeName = (TextView) view.findViewById(R.id.uebungListeName);//nameUebung-1
+        TextView uebungenListeDauer = (TextView) view.findViewById(R.id.uebungListeZeitaufwand);//Dauer-2
+
+
+
+
+        uebungenListeName.setText(cursor.getString(1));
+        uebungenListeDauer.setText(cursor.getString(2));
+
+        Log.i(TAG,uebungenListeName.getText().toString());
+
+
+
+        //TODO: Prüfen ob hier die von der Tabelle gesetzten ID übernommen wird oder die von Hand gesetzte.
+
+        idList.add(cursor.getInt(0));
+
+        //Log.i(TAG, "_id: " + idList.get(cursor.getInt(0)-1));
+
+
+
+        cursor.moveToNext();
+
+
+
+
+
+    }
+
+
+
+    @Override
+
+    public View newView(Context context, Cursor cursor, ViewGroup parent){
+
+        //Log.i(TAG, "newView");
+
+        //return inflator.inflate(R.layout.vertretungsplan2, null);
+
+        return LayoutInflater.from(context).inflate(R.layout.fragment_uebungen_liste_element, parent, false);
+
+    }
+
+
+
+    public ArrayList<Integer> getIds(){
+
+        return ids;
+
+    }
+
+}
