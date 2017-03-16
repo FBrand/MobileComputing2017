3d900afcc38fbe8ffbfe26c8943a8f39c965222c
Felix <FBrand@students.uni-mainz.de>
2017-03-16 11:04:24 +0100

    LocalDBConnection

---
+++ Connector/app/src/main/java/com/example/lars/connector/DBInfo.java
@@ -0,0 +1,47 @@
+package com.example.lars.connector;
+
+/**
+ * Created by lars on 09.03.17.
+ * Class containing all information about the local database.
+ */
+
+public class DBInfo {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "test2.db";
+
+    public static final String EXERCISE_TABLE_NAME = "exercise";
+    public static final String EXERCISE_COLUMN_NAME_ID = "_id";
+    public static final String EXERCISE_COLUMN_NAME_IDLOCAL = "idLocal";
+    public static final String EXERCISE_COLUMN_NAME_AGE = "age";
+    public static final String EXERCISE_COLUMN_NAME_AUTORMAIL = "autorMail";
+    public static final String EXERCISE_COLUMN_NAME_AUTORNAME = "autorName";
+    public static final String EXERCISE_COLUMN_NAME_DESCRIPTION = "description";
+    public static final String EXERCISE_COLUMN_NAME_RATING = "rating";
+    public static final String EXERCISE_COLUMN_NAME_DURATION = "duration";
+    public static final String EXERCISE_COLUMN_NAME_GRAPHIC = "graphic";
+    public static final String EXERCISE_COLUMN_NAME_GROUPSIZE = "groupsize";
+    public static final String EXERCISE_COLUMN_NAME_LASTCHANGE = "lastChange";
+    public static final String EXERCISE_COLUMN_NAME_MATIRIAL = "matirial";
+    public static final String EXERCISE_COLUMN_NAME_NAME = "name";
+    public static final String EXERCISE_COLUMN_NAME_KEYWORDS = "keywords";
+    public static final String EXERCISE_COLUMN_NAME_PHYSIS = "physis";
+    public static final String EXERCISE_COLUMN_NAME_TACTIC = "tactic";
+    public static final String EXERCISE_COLUMN_NAME_TECHNIC = "technic";
+    public static final String EXERCISE_COLUMN_NAME_SPORT = "sport";
+    public static final String EXERCISE_COLUMN_NAME_VIDEOLINK = "videolink";
+
+    public static final String TRAININGSUNIT_TABLE_NAME = "trainingsunit";
+    public static final String TRAININGSUNIT_COLUMN_NAME_ID = "_id";
+    public static final String TRAININGSUNIT_COLUMN_NAME_IDLOCAL = "idLocal";
+    public static final String TRAININGSUNIT_COLUMN_NAME_AGE = "age";
+    public static final String TRAININGSUNIT_COLUMN_NAME_AUTORMAIL = "autorMail";
+    public static final String TRAININGSUNIT_COLUMN_NAME_AUTORNAME = "autorName";
+    public static final String TRAININGSUNIT_COLUMN_NAME_DESCRIPTION = "description";
+    public static final String TRAININGSUNIT_COLUMN_NAME_RATING = "rating";
+    public static final String TRAININGSUNIT_COLUMN_NAME_DURATION = "duration";
+    public static final String TRAININGSUNIT_COLUMN_NAME_LASTCHANGE = "lastChange";
+    public static final String TRAININGSUNIT_COLUMN_NAME_NAME = "name";
+    public static final String TRAININGSUNIT_COLUMN_NAME_KEYWORDS = "keywords";
+    public static final String TRAININGSUNIT_COLUMN_NAME_EXERCISE = "exercise";
+}
---
+++ SCLerchenbergTrainerAssist/app/src/main/java/com/scltrainer/uni_mainz/sclerchenbergtrainerassist/GlobalDBConnection.java
@@ -0,0 +1,8 @@
+package com.scltrainer.uni_mainz.sclerchenbergtrainerassist;
+
+/**
+ * Created by Felix on 14.03.2017.
+ */
+
+public class GlobalDBConnection {
+}
